de7c01d8cab16c7328a204f6f29af1ad7c82c84a
Payペイ平 <kanethinmyth7575@ezweb.ne.jp>
2025-05-08 17:29:08 +0900

    retry

---
+++ src/app/sitemap.ts
@@ -0,0 +1,8 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://vercel.com/rookie-betts-projects/fill-fruitscoffee-w75b/9Rmms3zAYhFyaXcMAwtC18qhfJKP',
+      lastModified: new Date(),
+    },
+  ];
+}
