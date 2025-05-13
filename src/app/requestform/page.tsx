96ff4ec15f8a3688ebc235fbbe272eb88832b0d5
Payペイ平 <kanethinmyth7575@ezweb.ne.jp>
2025-05-13 22:26:32 +0900

    advance

--- src/app/requestform/page.tsx
+++ src/app/requestform/page.tsx
@@ -1,45 +1,58 @@
 'use client';
-import { useState } from 'react';
 import  H1tag  from "../components/H1tag";
+import Ptag from "../components/Ptag";
+import { useForm, ValidationError } from '@formspree/react';
 
-
+//name,email,message
 
 export default function Requestform() {
-    const [form, setForm] = useState({
-    name: '',
-    email: '',
-    phoneNum: '',
-    message: '',
-  });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const res = await fetch('/api/contact', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(form),
-    });
-
-    if (res.ok) {
-      alert('お問い合わせが無事、送信されました');
-    } else {
-      alert('送信に失敗しました');
-    }
-  };
-
+    const [state, handleSubmit] = useForm("xwpordwa");
+  if (state.succeeded) {
+      return <Ptag>貴重なご意見、ありがとうございました。</Ptag>;
+  }
 
   return (
       <div>  
       <section className="flex flex-col items-center mt-8 pb-16 mb-8">        
         <H1tag><i className="ri-mail-line  brownColor"></i>ご質問・お問い合わせ</H1tag>
         <form onSubmit={handleSubmit} className="max-w-md mx-auto space-y-4">
-          <input type="text" name="name" placeholder="お名前" required onChange={handleChange} className="border p-2 w-full" />
-          <input type="email" name="email" placeholder="メールアドレス" required onChange={handleChange} className="border p-2 w-full" />
-          <input type="tel" name="phone" placeholder="電話番号" required onChange={handleChange} className="border p-2 w-full" />
-          <textarea name="message" placeholder="お問い合わせ内容" required onChange={handleChange} className="border p-2 w-full h-32" />
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-200">送信</button>
+          <label htmlFor="name">
+            名前(ニックネームでも可)
+          </label>
+          <input id="name" type='name' name='name' className="border p-2 w-full"/>
+          <ValidationError 
+          prefix="Name" 
+          field="name"
+          errors={state.errors}
+          />
+          <label htmlFor="email">
+            メールアドレス
+          </label>
+          <input
+        id="email"
+        type="email" 
+        name="email"
+         className="border p-2 w-full"/>
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <label htmlFor="message">
+            お問い合わせ内容。ご不明な点、聞きたいこと
+      </label>
+      <textarea
+        id="message"
+        name="message"
+      className="border p-2 w-full h-32"/>
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}  className="bg-blue-500 text-white px-4 py-2 rounded">
+        ご確認の上、送信
+      </button>
         </form>
       </section>
     </div>
